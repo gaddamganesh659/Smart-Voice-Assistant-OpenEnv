7eaab7a4ff1c210e1b94a5eba5603993b22193b2
gaddamganesh659 <gaddamganesh659@gmail.com>
2026-03-29 12:49:36 +0530

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# This function tells the system how to start your environment
+def run_inference(env):
+    state = env.reset()
+    # Basic logic to show the environment is working
+    return state
+  
